60d4f89e83a1f174e74c97fefe3f864882af304b
YoKa-CEO <melnitskiy.i.i@gmail.com>
2017-10-15 18:57:38 +0300

    before getter

---
+++ src/com/company/AlcoCategory.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by yokaphone on 10/14/17.
+ */
+public enum AlcoCategory {
+    Wine,
+    StrongAlcogol,
+    Beer,
+    Liquor
+
+}
--- src/com/company/AlcoDrink.java
+++ src/com/company/AlcoDrink.java
@@ -4,4 +4,17 @@ package com.company;
  * Created by yokaphone on 10/14/17.
  */
 public class AlcoDrink {
+    private final String name = "";
+    private float price = 0;
+    private final AlcoCategory category = null;
+    private float amount = 0;
+    private final String alcoLower = "";
+    private int quantity = 0;
+
+    public AlcoDrink(String name, float price, AlcoCategory category, float amount, String alcoLower, int quantity){
+
+
+    }
+
+
 }
