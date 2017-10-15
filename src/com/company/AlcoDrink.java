66f27b894f83e6cfd56eb5c870126eb73917d903
YoKa-CEO <melnitskiy.i.i@gmail.com>
2017-10-15 19:16:23 +0300

    Add NonAlcDrink

--- src/com/company/AlcoDrink.java
+++ src/com/company/AlcoDrink.java
@@ -13,8 +13,49 @@ public class AlcoDrink {
 
     public AlcoDrink(String name, float price, AlcoCategory category, float amount, String alcoLower, int quantity){
 
+        name = this.name;
+        price = this.price;
+        category = this.category;
+        amount = this.amount;
+        alcoLower = this.alcoLower;
+        quantity = this.quantity;
+
 
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public float getPrice() {
+        return price;
+    }
+
+    public AlcoCategory getCategory() {
+        return category;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public String getAlcoLower() {
+        return alcoLower;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
---
+++ src/com/company/NonAlcDrink.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by yokaphone on 10/15/17.
+ */
+public class NonAlcDrink {
+}
